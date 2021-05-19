14e5767f3007ff22974bcc06ef5b5b5a943d94bf
michaelmumenthaler <michi.mumi@gmail.com>
2021-05-19 18:16:17 +0200

    initial commit

---
+++ SmallTicker.py
@@ -0,0 +1,66 @@
+import PySimpleGUI as sg
+import requests, time, json, os
+
+
+def loadSymbols():
+    symbols = json.load(open("symbols.json"))["symbols"]
+    return symbols
+
+
+symbolIndex = 0
+symbols = loadSymbols()
+url = f"https://api1.binance.com/api/v3/ticker/price?symbol="
+data = f"##########: ##########"
+textBox = [
+    sg.Text(
+        data,
+        key="-OUTPUT-",
+    )
+]
+
+layout = [textBox]
+
+# Create the window
+window = sg.Window(
+    "Ticker",
+    layout,
+    keep_on_top=True,
+    no_titlebar=True,
+    grab_anywhere=True,
+    return_keyboard_events=True,
+    location=(0, 0),
+)
+
+
+# Create an event loop
+counter = 0
+symbolCounter = 0
+
+while True:
+
+    # data = f"ADA: {json.loads(requests.get(url).text)['price'].replace('0000','')}"
+    if symbolCounter >= 20000:
+        symbols = loadSymbols()
+        symbolCounter = 0
+    else:
+        symbolCounter += 1
+
+    event, values = window.read(timeout=0.1)
+    if counter >= 1000:
+        data = f"{symbols[symbolIndex]}: {json.loads(requests.get(url + symbols[symbolIndex]).text)['price'].replace('0000','')}"
+        counter = 0
+
+    window["-OUTPUT-"](data)
+    counter += 1
+    if event == "f":
+        if symbolIndex >= len(symbols) - 1:
+            symbolIndex = 0
+            counter = 1000
+        else:
+            symbolIndex += 1
+            counter = 1000
+
+    if event == sg.WIN_CLOSED or event == "q":
+        break
+
+window.close()
